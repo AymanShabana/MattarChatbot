d9d0824ec9aad3da7b91046237249b25785f0a6f
Ayman Shabana <53878617+AymanShabana@users.noreply.github.com>
2020-07-14 18:06:16 -0700

    Code cleaned up

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
 from rivescript import RiveScript
-import sqlite3
-import arrow
 
-#db = sqlite3.connect('attendance.db')
-#c = db.cursor()
-#c.execute('''DROP TABLE attendance''')
-#db.commit()
-#c.execute('''CREATE TABLE attendance
-#             (name text, year text, date DATE)''')
-#c.execute("INSERT INTO attendance VALUES ('Essam H','Junior','"+str(arrow.now().format('YYYY-MM-DD'))+"')")
-#db.commit()
-#t = ('Essam H',)
-#c.execute("SELECT * FROM attendance WHERE name=? AND date='"+str(arrow.now().format('YYYY-MM-DD'))+"'", t)
-#print(c.fetchone())
-#t = ('Ayman Yasser',)
-#c.execute("SELECT * FROM attendance WHERE name=? AND date='"+str(arrow.now().format('YYYY-MM-DD'))+"'", t)
-#print(c.fetchone())
-#db.close()
 
 bot = RiveScript()
 bot.load_directory("./brain")
